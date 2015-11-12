dc5e12e4f7f01c53c22c83ec6eb96ba8659a7ea7
casalgado <casalgado86@yahoo.com>
2015-11-12 08:44:36 +0100

    using jquery

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,31 @@
 
-getElementById("sq1").addEventListener("click", function(){
-    alert("Hello World!");
-});
-alert("Hello World!");
+
+
+var ready = function() {
+
+	$(document).ready(function() {
+	  	$(".square").on("click", function() {
+	  		$(".square").removeClass("selected")
+	  			$(this).addClass("selected")
+
+	  		if ($(this).attr("id") == "sq1") {
+	  			$("#response").html("Good Choice!")
+	  		} else if ($(this).attr("id") == "sq2") {
+	  			$("#response").html("I hope they have Mint")
+	  		} else {
+	  			$("#response").html("Proceed at your own Risk")
+	  		} 
+	    })
+
+	  	$("#title").on("click", function() {
+	  		$(".square").removeClass("selected")
+	  		$("#response").html(" ")
+	    })
+
+	}); 
+};
+
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
