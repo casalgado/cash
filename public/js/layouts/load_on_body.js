f78998430b1022ce1719f87840187c7028a0f57b
casalgado <casalgado86@yahoo.com>
2016-06-18 16:42:28 +0200

    added js/layouts to simplify creating new pages

---
+++ public/js/layouts/load_on_body.js
@@ -0,0 +1,25 @@
+document.write('\
+	<!-- Scroll-up -->\
+	<div class="scroll-up">\
+		<a href="#totop"><i class="fa fa-angle-double-up"></i></a>\
+	</div>\
+	<!-- Javascript files -->\
+	<script src="js/semantic/jquery-2.1.3.min.js"></script>\
+	<script src="js/semantic/bootstrap.min.js"></script>\
+	<script src="js/semantic/jquery.mb.YTPlayer.min.js"></script>\
+	<script src="js/semantic/appear.js"></script>\
+	<script src="js/semantic/jquery.simple-text-rotator.min.js"></script>\
+	<script src="js/semantic/jqBootstrapValidation.js"></script>\
+	<script src="js/semantic/gmaps.js"></script>\
+	<script src="js/semantic/isotope.pkgd.min.js"></script>\
+	<script src="js/semantic/imagesloaded.pkgd.js"></script>\
+	<script src="js/semantic/jquery.flexslider-min.js"></script>\
+	<script src="js/semantic/jquery.magnific-popup.min.js"></script>\
+	<script src="js/semantic/jquery.fitvids.js"></script>\
+	<script src="js/semantic/smoothscroll.js"></script>\
+	<script src="js/semantic/wow.min.js"></script>\
+	<script src="js/semantic/contact.js"></script>\
+	<script src="js/semantic/custom.js"></script>\
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>\
+');
+
---
+++ public/js/layouts/load_on_head.js
@@ -0,0 +1,31 @@
+document.write('\
+\
+	<!-- added by me -->\
+\
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>\
+\
+	<!-- semantic starts here -->\
+\
+	<meta charset="UTF-8">\
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">\
+	<meta name="description" content="">\
+	<meta name="author" content="">\
+	\
+\
+	<!-- Bootstrap core CSS -->\
+	<link href="css/semantic/bootstrap.min.css" rel="stylesheet">\
+	\
+	<!-- Plugins -->\
+	<link href="css/semantic/simpletextrotator.css" rel="stylesheet">\
+	<link href="css/semantic/font-awesome.min.css" rel="stylesheet">\
+	<link href="css/semantic/et-line-font.css" rel="stylesheet">\
+	<link href="css/semantic/magnific-popup.css" rel="stylesheet">\
+	<link href="css/semantic/flexslider.css" rel="stylesheet">\
+	<link href="css/semantic/animate.css" rel="stylesheet">\
+	\
+	<!-- Template core CSS -->\
+	<link href="css/semantic/style.css" rel="stylesheet">\
+	\
+	<!-- Custom css -->\
+	<link href="css/semantic/custom.css" rel="stylesheet">'
+);
